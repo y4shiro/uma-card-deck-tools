150262aacaf8fbadf591de9ba1098eece4c5beab
y4shiro <y4shironao@gmail.com>
2022-07-02 00:22:36 +0900

    Header コンポーネント追加

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const Header = (): JSX.Element => {
+  return (
+    <Box w='100%' h='16' bgColor='green.500' alignContent='center'>
+      <Text>Header Components</Text>
+    </Box>
+  );
+};
+
+export default Header;
