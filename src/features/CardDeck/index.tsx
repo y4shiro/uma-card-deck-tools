369837a8068a50febd97d6a541bd9481eddee7e1
y4shiro <y4shironao@gmail.com>
2022-07-02 18:12:11 +0900

    feat: CardDeck コンポーネントのスタイル実装

--- src/features/CardDeck/index.tsx
+++ src/features/CardDeck/index.tsx
@@ -1,7 +1,27 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Center, Grid, GridItem } from '@chakra-ui/react';
 
 const CardDeck = (): JSX.Element => {
-  return <Box>CardDeck</Box>;
+  const cardDeck = ['1', '2', '3', '4', '5', '6'];
+
+  return (
+    <Box bgColor='blue.100'>
+      <Grid w='100%' h='100%' p='4' gap='4' templateColumns='repeat(3, 1fr)'>
+        {cardDeck.map((value, index) => (
+          <Card value={value} key={index} />
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+const Card = ({ value }: { value: string }): JSX.Element => {
+  return (
+    <GridItem h='240px' w='180px' borderRadius='16' bgColor='gray.400'>
+      <Center h='100%' w='100%'>
+        {value}
+      </Center>
+    </GridItem>
+  );
 };
 
 export default CardDeck;
