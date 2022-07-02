029554bf1f896ce9209e8ed0cd443f7ae1779df6
y4shiro <y4shironao@gmail.com>
2022-07-02 23:10:19 +0900

    feat: Header にトップページと about ページのリンクナビゲーションを追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,18 @@
 import { HStack, Link, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 const Header = (): JSX.Element => {
   return (
     <HStack as='nav' w='100%' h='16' p='4' bgColor='green.500' justify='space-between'>
       <Text>Header Components</Text>
-      <Link>About</Link>
+      <HStack gap='4'>
+        <NextLink href='/'>
+          <Link>Top</Link>
+        </NextLink>
+        <NextLink href='/about'>
+          <Link>About</Link>
+        </NextLink>
+      </HStack>
     </HStack>
   );
 };
