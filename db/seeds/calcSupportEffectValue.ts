4c5023878e7e9ec45e3e46e191b0041a7c7d0caa
y4shiro <y4shironao@gmail.com>
2022-09-09 10:49:38 +0900

    refactor: getValue で計算した結果を { label: value } の型で返すよう変更

--- db/seeds/calcSupportEffectValue.ts
+++ db/seeds/calcSupportEffectValue.ts
@@ -49,8 +49,14 @@ const data: dataType[] = [
   },
 ];
 
-const getValue = (effect: dataType, cur: typeof EFFECT_LIMITS[number]) => {
-  if (effect[cur] !== -1) return console.log(`${cur} : ${effect[cur]}`); // カラムに -1 以外の数値が入っている場合はそのまま返す
+const getValue = (
+  effect: dataType,
+  cur: typeof EFFECT_LIMITS[number],
+): { label: typeof EFFECT_LIMITS[number]; value: number } => {
+  if (effect[cur] !== -1) {
+    const val = effect[cur];
+    return { label: cur, value: val };
+  } // カラムに -1 以外の数値が入っている場合はそのまま返す
 
   const index = EFFECT_LIMITS.indexOf(cur); // 計算したいカラムの順番、例えば "lv25" なら 5
   let min: number = 0; // index 0 番目から計算したいカラムに含まれる数値の最小値
@@ -76,21 +82,25 @@ const getValue = (effect: dataType, cur: typeof EFFECT_LIMITS[number]) => {
 
   // 最小値が最大値以上、最大値と最小値の差分が 1、最小値が 0 のいずれかの場合は min を返す
   if (max <= min || max - min === 1 || min === 0) {
-    console.log(`${cur} : ${min}`);
+    return { label: cur, value: min };
   } else {
     const result = Math.floor(
       min +
         ((max - min) * (EFFECT_LIMITS.indexOf(cur) - EFFECT_LIMITS.indexOf(min_limit))) /
           (EFFECT_LIMITS.indexOf(max_limit) - EFFECT_LIMITS.indexOf(min_limit)),
     );
-    console.log(`${cur} : ${result}`);
+    return { label: cur, value: result };
   }
 };
 
 const main = () => {
   data.map((effect) => {
+    console.log(`id: ${effect['id']}`);
+    console.log(`type: ${effect['type']}`);
+
     EFFECT_LIMITS.map((limit_vallue) => {
-      getValue(effect, limit_vallue);
+      const result = getValue(effect, limit_vallue);
+      console.log(result);
     });
   });
 };
