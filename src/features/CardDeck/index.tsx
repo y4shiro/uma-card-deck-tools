bafc7f92eb72370be943ea4e9d62fa6ba2352431
y4shiro <y4shironao@gmail.com>
2022-09-21 18:28:02 +0900

    feat: サポカデータの JSON の型定義を types/cards.ts に分割

--- src/features/CardDeck/index.tsx
+++ src/features/CardDeck/index.tsx
@@ -2,51 +2,8 @@ import { Box, Grid } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
 import Card from './Card';
+import type { CardType } from '@/types/cards';
 import { supabase } from '@/utils/supabaseClient';
-
-type Effects = {
-  id: number;
-  name: string;
-  values: EffectValue[];
-};
-
-type EffectValue = {
-  level: number;
-  value: number;
-};
-
-type EventSkill = {
-  id: number;
-  name: string;
-  skill_pt: number | null;
-  img_path: string | null;
-};
-
-type TrainingSkill = {
-  id: number;
-  name: string;
-  skill_pt: number | null;
-  img_path: string | null;
-};
-
-type StatusGain = {
-  id: number;
-  name: string;
-  value: number;
-};
-
-type CardType = {
-  card_id: number;
-  card_name: string;
-  charactor_name: string;
-  card_rarity: 'R' | 'SR' | 'SSR';
-  card_type: 'Speed' | 'Stamina' | 'Power' | 'Guts' | 'Wisdom' | 'Friends' | 'Group';
-  card_img_path: string | null;
-  effects: Effects[];
-  event_skills?: EventSkill[];
-  training_skills?: TrainingSkill[];
-  status_gains?: StatusGain[];
-};
 
 const CardDeck = (): JSX.Element => {
   const cardDeck = ['1', '2', '3', '4', '5', '6'];
---
+++ src/types/cards.ts
@@ -0,0 +1,43 @@
+export type CardType = {
+  card_id: number;
+  card_name: string;
+  charactor_name: string;
+  card_rarity: 'R' | 'SR' | 'SSR';
+  card_type: 'Speed' | 'Stamina' | 'Power' | 'Guts' | 'Wisdom' | 'Friends' | 'Group';
+  card_img_path: string | null;
+  effects: Effects[];
+  event_skills?: EventSkill[];
+  training_skills?: TrainingSkill[];
+  status_gains?: StatusGain[];
+};
+
+export type Effects = {
+  id: number;
+  name: string;
+  values: EffectValue[];
+};
+
+export type EffectValue = {
+  level: number;
+  value: number;
+};
+
+export type EventSkill = {
+  id: number;
+  name: string;
+  skill_pt: number | null;
+  img_path: string | null;
+};
+
+export type TrainingSkill = {
+  id: number;
+  name: string;
+  skill_pt: number | null;
+  img_path: string | null;
+};
+
+export type StatusGain = {
+  id: number;
+  name: string;
+  value: number;
+};
