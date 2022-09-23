8e11488cde77e914401cd256f67d5bc8873ad50b
y4shiro <y4shironao@gmail.com>
2022-09-23 20:00:36 +0900

    feat: Card の枠色をレアリティ毎に変更

--- src/features/CardDeck/Card/index.tsx
+++ src/features/CardDeck/Card/index.tsx
@@ -1,4 +1,4 @@
-import { chakra, GridItem } from '@chakra-ui/react';
+import { chakra, GridItem, theme } from '@chakra-ui/react';
 import Image from 'next/image';
 
 import type { CardType } from '@/types/cards';
@@ -8,15 +8,30 @@ const ChakraNextImage = chakra(Image);
 type Props = { card: CardType; key: number };
 
 const Card: React.FC<Props> = (props) => {
+  let bdColor: string;
+
+  switch (props.card.card_rarity) {
+    case 'R':
+      bdColor = theme.colors.gray[200];
+      break;
+    case 'SR':
+      bdColor = theme.colors.yellow[400];
+      break;
+    case 'SSR':
+      bdColor = theme.colors.purple[500];
+      break;
+  }
+
   return (
     <GridItem
       h='240px'
       w='180px'
       position='relative'
       overflow='hidden'
-      borderColor='#F9DD7D'
+      borderColor={bdColor}
       borderWidth='6px'
       borderRadius='16'
+      shadow='md'
     >
       <ChakraNextImage
         layout='fill'
