5324055ee29d658438bd7299a8573d3536a8b106
y4shiro <y4shironao@gmail.com>
2022-09-27 10:47:21 +0900

    feat: カードコンポーネントを作り直すため、装飾を一旦削除し、next/image のみを利用してカード表示するシンプルな方式に戻した

--- src/features/CardDeck/Card/index.tsx
+++ src/features/CardDeck/Card/index.tsx
@@ -18,58 +18,16 @@ const Card: React.FC<Props> = ({ card }) => {
   };
 
   return (
-    <Box
-      position='relative'
-      h='240px'
-      w='180px'
-      padding='6px'
-      bgGradient={bgGradients[card.card_rarity]}
-      borderRadius='20'
-      borderWidth='1px'
-      borderColor={outerBorderColors[card.card_rarity]}
-      shadow='md'
-    >
-      <Box
-        position='relative'
-        h='full'
-        w='full'
-        borderRadius='16'
-        borderWidth='1px'
-        borderColor='#ccc'
-        overflow='hidden'
-      >
-        <Image
-          quality={'100'}
-          layout='fill'
-          objectFit='cover'
-          placeholder='blur'
-          blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOcXg8AAbMBGIpVIK8AAAAASUVORK5CYII='
-          style={{ transition: '0.2s' }}
-          src={`w_180,h_240/uma-support-card/card-images/${card.card_img_path}`}
-          alt={`サポートカード "${card.card_name}" の画像`}
-        />
-        {/* <Box w='full' h='full' bgColor='blackAlpha.500'></Box> */}
-      </Box>
-
-      <Box position='absolute' top='0' right='0'>
-        <Image
-          height='44px'
-          width='44px'
-          src={`uma-support-card/card-type-icons/${card.card_type}.png`}
-          alt={`サポートカード "${card.card_name}" のタイプアイコン`}
-        />
-      </Box>
-
-      <Box position='absolute' top='0' left='8px'>
-        <Image
-          height='44px'
-          width='48px'
-          quality={100}
-          src={`uma-support-card/card-rarity-icons/${card.card_rarity}.png`}
-          alt={`サポートカード "${card.card_name}" のレアリティアイコン`}
-        />
-      </Box>
-    </Box>
+    <Image
+      quality={'100'}
+      width='180px'
+      height='240px'
+      placeholder='blur'
+      blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOcXg8AAbMBGIpVIK8AAAAASUVORK5CYII='
+      // style={{ transition: '0.2s' }}
+      src={`/uma-support-card/card-images/${card.card_img_path}`}
+      alt={`サポートカード "${card.card_name}" の画像`}
+    />
   );
 };
 
