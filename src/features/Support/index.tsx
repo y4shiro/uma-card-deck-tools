147b88fbd2790916ac6b377249507258530c328e
y4shiro <y4shironao@gmail.com>
2022-10-11 19:44:17 +0900

    refactor: Support/index.tsx の型定義を指定

--- src/features/Support/index.tsx
+++ src/features/Support/index.tsx
@@ -1,11 +1,10 @@
 import { VStack } from '@chakra-ui/react';
-import React from 'react';
 
 import CardDeck from './CardDeck';
 import Skills from './Skills';
 import Status from './Status';
 
-const Support = () => {
+const Support: React.FC = () => {
   return (
     <VStack w='100%' gap={{ base: 2, sm: 4 }}>
       <CardDeck />
