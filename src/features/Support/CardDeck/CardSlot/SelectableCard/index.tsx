aff020629c23991a2df26fe6fe5a215d440ec606
y4shiro <y4shironao@gmail.com>
2022-10-19 22:31:54 +0900

    feat: SelectableCard にて、既に CardModal に編成済みのため選択不可能なカードの JSX を新たなコンポーネントとして分割

--- src/features/Support/CardDeck/CardSlot/SelectableCard/index.tsx
+++ src/features/Support/CardDeck/CardSlot/SelectableCard/index.tsx
@@ -25,29 +25,7 @@ const SelectableCard: React.FC<Props> = ({ card, imgSize, selectedCards }) => {
     dispatch(closeModal());
   };
 
-  if (alreadySelectedCard)
-    return (
-      <Box position='relative'>
-        <Box filter='auto' brightness='40%'>
-          <Card card={card} imgSize={imgSize} />
-        </Box>
-        <Box
-          position='absolute'
-          top='-2%'
-          right='12%'
-          left='12%'
-          margin='auto'
-          px={{ base: '2px', md: '6px', lg: '12px' }}
-          borderRadius='12px'
-          textColor='white'
-          background='#E4436B'
-        >
-          <Text fontSize={{ base: '12px', sm: '14px', md: '16px', lg: '20px' }} fontWeight='bold'>
-            設定中
-          </Text>
-        </Box>
-      </Box>
-    );
+  if (alreadySelectedCard) return <AlreadySelectedCard card={card} imgSize={imgSize} />;
 
   return (
     <Box
@@ -57,6 +35,31 @@ const SelectableCard: React.FC<Props> = ({ card, imgSize, selectedCards }) => {
       onClick={() => changeHandler({ slotId: slotId!, cardId: card.card_id })}
     >
       <Card card={card} imgSize={imgSize} />
+    </Box>
+  );
+};
+
+const AlreadySelectedCard: React.FC<Pick<Props, 'card' | 'imgSize'>> = ({ card, imgSize }) => {
+  return (
+    <Box position='relative'>
+      <Box filter='auto' brightness='40%'>
+        <Card card={card} imgSize={imgSize} />
+      </Box>
+      <Box
+        position='absolute'
+        top='-2%'
+        right='12%'
+        left='12%'
+        margin='auto'
+        px={{ base: '2px', md: '6px', lg: '12px' }}
+        borderRadius='12px'
+        textColor='white'
+        background='#E4436B'
+      >
+        <Text fontSize={{ base: '12px', sm: '14px', md: '16px', lg: '20px' }} fontWeight='bold'>
+          設定中
+        </Text>
+      </Box>
     </Box>
   );
 };
