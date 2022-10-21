71129dbcb4e56fd56a8d559bf488d7f036207205
y4shiro <y4shironao@gmail.com>
2022-10-21 16:13:26 +0900

    feat: CardFilter にチェックボッスを追加

--- src/features/Support/CardDeck/CardModal/CardFilter/index.tsx
+++ src/features/Support/CardDeck/CardModal/CardFilter/index.tsx
@@ -1,11 +1,29 @@
-import { Box } from '@chakra-ui/react';
+import { Checkbox, CheckboxGroup, HStack } from '@chakra-ui/react';
 import React from 'react';
 
 const CardFilter: React.FC = () => {
   return (
-    <Box my={4} p={4} bgColor='white' borderRadius='16px'>
-      card filter
-    </Box>
+    <HStack w='100%' my={4} p={4} bgColor='white' borderRadius='16px'>
+      <CheckboxGroup>
+        <HStack bgColor='red.100'>
+          <Checkbox>スピード</Checkbox>
+          <Checkbox>パワー</Checkbox>
+          <Checkbox>スタミナ</Checkbox>
+          <Checkbox>根性</Checkbox>
+          <Checkbox>賢さ</Checkbox>
+          <Checkbox>友人</Checkbox>
+          <Checkbox>グループ</Checkbox>
+        </HStack>
+      </CheckboxGroup>
+
+      <CheckboxGroup>
+        <HStack bgColor='blue.100'>
+          <Checkbox>SSR</Checkbox>
+          <Checkbox>SR</Checkbox>
+          <Checkbox>R</Checkbox>
+        </HStack>
+      </CheckboxGroup>
+    </HStack>
   );
 };
 
