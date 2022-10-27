eb876bf5f2e01b9686e900560ae4dad434b8855b
y4shiro <y4shironao@gmail.com>
2022-10-27 17:54:10 +0900

    feat: Skills コンポーネントにて、Deck にセットされたカードのイベント取得スキルを取得して表示出来るよう実装

--- src/features/Support/Skills/index.tsx
+++ src/features/Support/Skills/index.tsx
@@ -1,14 +1,34 @@
 import { Box, Grid, Text, VStack } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+
+import { RootState } from '@/app/store';
+import { useGetCardsQuery } from '@/services/card';
 
 const Skills = (): JSX.Element => {
-  const skills = ['1', '2', '3', '4'];
+  const { data, error, isLoading } = useGetCardsQuery();
+  const deck = useSelector((state: RootState) => state.cardDeck);
+  const deckCardidLists = deck.filter((card) => card.cardId).flatMap((card) => card.cardId);
+
+  const deckCardSkillsLists = data
+    ?.filter((card) => deckCardidLists.includes(card.card_id))
+    .map((card) => {
+      return {
+        card_id: card.card_id,
+        card_name: card.card_name,
+        event_skills: card.event_skills,
+      };
+    });
 
   return (
     <VStack w='100%' bgColor='blue.100'>
       <Text>Skills</Text>
-      {skills.map((s, index) => (
-        <Text key={index}>{s}</Text>
-      ))}
+      {deckCardSkillsLists?.map((card) =>
+        card.event_skills?.map((skill) => (
+          <Text key={skill.name}>
+            {card.card_name}:{skill.name}
+          </Text>
+        )),
+      )}
     </VStack>
   );
 };
