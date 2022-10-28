ef0e8345938c17505eda6c2be93f3f6d9f14e5d7
y4shiro <y4shironao@gmail.com>
2022-10-28 17:36:49 +0900

    feat: Skills コンポーネントにて、スキルPt を表示するよう変更

--- src/features/Support/Skills/SkillItem/index.tsx
+++ src/features/Support/Skills/SkillItem/index.tsx
@@ -1,16 +1,28 @@
-import { Text } from '@chakra-ui/react';
+import { Box, HStack, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 type Props = {
   skillName: string;
+  skillPt: number | null;
   cardName: string;
 };
 
-const SkillItem: React.FC<Props> = ({ skillName, cardName }) => {
+const SkillItem: React.FC<Props> = ({ skillName, skillPt, cardName }) => {
   return (
-    <Text>
-      {cardName}:{skillName}
-    </Text>
+    <HStack w='480px' p='4' borderRadius='8' bgColor='white'>
+      <Box boxSize='48px' bgColor='gray.200'>
+        icon
+      </Box>
+
+      <VStack w='100%' alignContent='start'>
+        <Text>{cardName}</Text>
+
+        <HStack>
+          <Text>{skillName}</Text>
+          {skillPt && <Text>SkillPoint:{skillPt}</Text>}
+        </HStack>
+      </VStack>
+    </HStack>
   );
 };
 
--- src/features/Support/Skills/index.tsx
+++ src/features/Support/Skills/index.tsx
@@ -23,22 +23,32 @@ const Skills = (): JSX.Element => {
     });
 
   return (
-    <VStack w='100%' gap='4' bgColor='blue.100'>
+    <VStack w='100%' p='4' gap='4' bgColor='blue.100'>
       <Text fontSize='4xl'>Skills</Text>
-      <VStack>
+      <VStack w='100%' p='4' bgColor='blue.200'>
         <Text fontSize='2xl'>イベント取得スキル</Text>
         {deckCardSkillsLists?.map((card) =>
           card.event_skills?.map((skill) => (
-            <SkillItem key={skill.name} skillName={skill.name} cardName={card.card_name} />
+            <SkillItem
+              key={skill.name}
+              skillName={skill.name}
+              skillPt={skill.skill_pt}
+              cardName={card.card_name}
+            />
           )),
         )}
       </VStack>
 
-      <VStack>
+      <VStack w='100%' p='4' bgColor='blue.200'>
         <Text fontSize='2xl'>トレーニング取得スキル</Text>
         {deckCardSkillsLists?.map((card) =>
           card.training_skills?.map((skill) => (
-            <SkillItem key={skill.name} skillName={skill.name} cardName={card.card_name} />
+            <SkillItem
+              key={skill.name}
+              skillName={skill.name}
+              skillPt={skill.skill_pt}
+              cardName={card.card_name}
+            />
           )),
         )}
       </VStack>
