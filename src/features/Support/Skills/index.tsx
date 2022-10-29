032d45678220897fb342166c96f935ef44080a61
y4shiro <y4shironao@gmail.com>
2022-10-29 22:06:29 +0900

    refactor: Skills コンポーネントを分割して SkillList コンポーネントを新たに追加

--- src/features/Support/Skills/index.tsx
+++ src/features/Support/Skills/index.tsx
@@ -1,7 +1,7 @@
-import { Heading, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { Heading, VStack } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 
-import SkillItem from './SkillItem';
+import SkillList from './SkillList';
 
 import { RootState } from '@/app/store';
 import { useGetCardsQuery } from '@/services/card';
@@ -11,58 +11,31 @@ const Skills = (): JSX.Element => {
   const deck = useSelector((state: RootState) => state.cardDeck);
   const deckCardidLists = deck.filter((card) => card.cardId).flatMap((card) => card.cardId);
 
-  const deckCardSkillsLists = data
+  const cardEventSkills = data
     ?.filter((card) => deckCardidLists.includes(card.card_id))
     .map((card) => {
       return {
         card_id: card.card_id,
         card_name: card.card_name,
-        event_skills: card.event_skills,
-        training_skills: card.training_skills,
+        card_skills: card.event_skills,
+      };
+    });
+
+  const cardTrainingSkills = data
+    ?.filter((card) => deckCardidLists.includes(card.card_id))
+    .map((card) => {
+      return {
+        card_id: card.card_id,
+        card_name: card.card_name,
+        card_skills: card.training_skills,
       };
     });
 
   return (
     <VStack w='100%' py='2' px={{ base: '0', md: '2' }} gap='4' bgColor='blue.100'>
       <Heading fontSize='4xl'>Skills</Heading>
-
-      <VStack w='100%' p='4' gap='2' bgColor='blue.200'>
-        <Heading as='h2' fontSize='2xl'>
-          イベント取得スキル
-        </Heading>
-
-        <SimpleGrid w='100%' columns={{ base: 1, md: 2 }} gap={{ base: '2', md: '4' }}>
-          {deckCardSkillsLists?.map((card) =>
-            card.event_skills?.map((skill) => (
-              <SkillItem
-                key={skill.name}
-                skillName={skill.name}
-                skillPt={skill.skill_pt}
-                cardName={card.card_name}
-              />
-            )),
-          )}
-        </SimpleGrid>
-      </VStack>
-
-      <VStack w='100%' p='4' gap='2' bgColor='blue.200'>
-        <Heading as='h2' fontSize='2xl'>
-          トレーニング取得スキル
-        </Heading>
-
-        <SimpleGrid w='100%' columns={{ base: 1, md: 2 }} gap={{ base: '2', md: '4' }}>
-          {deckCardSkillsLists?.map((card) =>
-            card.training_skills?.map((skill) => (
-              <SkillItem
-                key={skill.name}
-                skillName={skill.name}
-                skillPt={skill.skill_pt}
-                cardName={card.card_name}
-              />
-            )),
-          )}
-        </SimpleGrid>
-      </VStack>
+      <SkillList heading={'イベント取得スキル'} skillLists={cardEventSkills} />
+      <SkillList heading={'トレーニング取得スキル'} skillLists={cardTrainingSkills} />
     </VStack>
   );
 };
