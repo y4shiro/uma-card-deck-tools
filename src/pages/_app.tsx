fa860c5c89d732d55271a7cd454757f640299037
y4shiro <y4shironao@gmail.com>
2022-10-31 17:37:25 +0900

    feat: ページ全体のフォントを指定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppPropsWithLayout } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 
+import theme from '@/styles/theme';
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
     <Provider store={store}>
-      <ChakraProvider>{getLayout(<Component {...pageProps} />)}</ChakraProvider>
+      <ChakraProvider theme={theme}>{getLayout(<Component {...pageProps} />)}</ChakraProvider>
     </Provider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif`,
+    body: `"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif`,
+  },
+});
+
+export default theme;
