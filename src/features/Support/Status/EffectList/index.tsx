79ac0af664f10a56f3003e9fb6e9ba46e5680088
y4shiro <y4shironao@gmail.com>
2022-11-11 18:48:05 +0900

    feat: EffectList の表示内容を変更し、カード名を削除してカードアイコンを追加

--- src/features/Support/Status/EffectList/index.tsx
+++ src/features/Support/Status/EffectList/index.tsx
@@ -1,4 +1,6 @@
-import { Text, HStack, VStack } from '@chakra-ui/react';
+import { Text, HStack, VStack, Center } from '@chakra-ui/react';
+import Image from 'next/image';
+import React from 'react';
 
 import { EffectListType } from '..';
 
@@ -11,25 +13,29 @@ const EffectList: React.FC<Props> = ({ effectList }) => {
     <VStack w='100%' p='4' gap='1'>
       {effectList.map((effect) => {
         return (
-          <>
-            {[...effect.values].map(
-              ([_k, v]) =>
-                v.effect_value > 0 && (
-                  <HStack
-                    w='100%'
-                    p='4'
-                    bgColor='white'
-                    borderRadius='8'
-                    shadow='md'
-                    key={`${effect.name},${v.card_id}`}
-                  >
-                    <Text>{effect.name}</Text>
-                    <Text>{v.card_name}</Text>
-                    <Text>{v.effect_value}</Text>
-                  </HStack>
-                ),
-            )}
-          </>
+          <React.Fragment key={`${effect.name}`}>
+            {[...effect.values].map(([_k, v]) => (
+              <HStack
+                w='100%'
+                p='4'
+                bgColor='white'
+                borderRadius='8'
+                shadow='md'
+                key={`${effect.name},${v.card_id}`}
+              >
+                <Text>{effect.name}</Text>
+                <Text>{v.effect_value}</Text>
+                <Center boxSize={{ base: '16px', md: '36px' }}>
+                  <Image
+                    width='64px'
+                    height='64px'
+                    src={`/uma-support-card/card-icons/${v.card_id}.png`}
+                    alt='サポートカードのアイコン'
+                  />
+                </Center>
+              </HStack>
+            ))}
+          </React.Fragment>
         );
       })}
     </VStack>
