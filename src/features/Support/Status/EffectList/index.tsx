45c514a3574c135429ac06c60573de5f6dcff817
y4shiro <y4shironao@gmail.com>
2022-11-11 17:47:23 +0900

    feat: EffectList のスタイルをある程度設定

--- src/features/Support/Status/EffectList/index.tsx
+++ src/features/Support/Status/EffectList/index.tsx
@@ -8,17 +8,27 @@ type Props = {
 
 const EffectList: React.FC<Props> = ({ effectList }) => {
   return (
-    <VStack>
+    <VStack w='100%' p='4' gap='1'>
       {effectList.map((effect) => {
         return (
           <>
-            {[...effect.values].map(([_k, v]) => (
-              <HStack key={`${effect.name},${v.card_id}`}>
-                <Text>{effect.name}</Text>
-                <Text>{v.card_name}</Text>
-                <Text>{v.effect_value}</Text>
-              </HStack>
-            ))}
+            {[...effect.values].map(
+              ([_k, v]) =>
+                v.effect_value > 0 && (
+                  <HStack
+                    w='100%'
+                    p='4'
+                    bgColor='white'
+                    borderRadius='8'
+                    shadow='md'
+                    key={`${effect.name},${v.card_id}`}
+                  >
+                    <Text>{effect.name}</Text>
+                    <Text>{v.card_name}</Text>
+                    <Text>{v.effect_value}</Text>
+                  </HStack>
+                ),
+            )}
           </>
         );
       })}
