e9a1cd17ab5fdca8173a476fa66f393adf609b3d
y4shiro <y4shironao@gmail.com>
2022-11-13 18:23:29 +0900

    feat: cardDeckSlice の状態に card 単体のデータを追加

--- src/features/Support/CardDeck/cardDeckSlice.ts
+++ src/features/Support/CardDeck/cardDeckSlice.ts
@@ -2,27 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
 import { RootState } from '@/app/store';
+
 import type { CardSlotType, SlotId } from '@/types/cardSlot';
 
 const initialState: CardSlotType[] = [
-  { slotId: 0, cardId: null, belongCharaIds: [] },
-  { slotId: 1, cardId: null, belongCharaIds: [] },
-  { slotId: 2, cardId: null, belongCharaIds: [] },
-  { slotId: 3, cardId: null, belongCharaIds: [] },
-  { slotId: 4, cardId: null, belongCharaIds: [] },
-  { slotId: 5, cardId: null, belongCharaIds: [] },
+  { slotId: 0, cardId: null, cardData: null, belongCharaIds: [] },
+  { slotId: 1, cardId: null, cardData: null, belongCharaIds: [] },
+  { slotId: 2, cardId: null, cardData: null, belongCharaIds: [] },
+  { slotId: 3, cardId: null, cardData: null, belongCharaIds: [] },
+  { slotId: 4, cardId: null, cardData: null, belongCharaIds: [] },
+  { slotId: 5, cardId: null, cardData: null, belongCharaIds: [] },
 ];
 export const cardDeckSlice = createSlice({
   name: 'cardDeck',
   initialState,
   reducers: {
     changeCard: (state, action: PayloadAction<CardSlotType>) => {
-      const { slotId, cardId, belongCharaIds } = action.payload;
-      state[slotId] = { slotId, cardId, belongCharaIds };
+      const { slotId, cardId, cardData, belongCharaIds } = action.payload;
+      state[slotId] = { slotId, cardId, cardData, belongCharaIds };
     },
     removeCard: (state, action: PayloadAction<{ slotId: SlotId }>) => {
       const { slotId } = action.payload;
-      state[slotId] = { slotId, cardId: null, belongCharaIds: [] };
+      state[slotId] = { slotId, cardId: null, cardData: null, belongCharaIds: [] };
     },
   },
 });
