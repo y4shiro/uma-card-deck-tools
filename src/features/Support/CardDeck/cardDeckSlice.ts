4f79be879d0a4f30a86a47dcbb9b9d30018c15bd
y4shiro <y4shironao@gmail.com>
2022-11-14 16:58:35 +0900

    feat: サポカの限界突破数を管理する limitBreakSteps を cardDeckSlice に追加

--- src/features/Support/CardDeck/cardDeckSlice.ts
+++ src/features/Support/CardDeck/cardDeckSlice.ts
@@ -6,24 +6,31 @@ import { RootState } from '@/app/store';
 import type { CardSlotType, SlotId } from '@/types/cardSlot';
 
 const initialState: CardSlotType[] = [
-  { slotId: 0, cardId: null, cardData: null, belongCharaIds: [] },
-  { slotId: 1, cardId: null, cardData: null, belongCharaIds: [] },
-  { slotId: 2, cardId: null, cardData: null, belongCharaIds: [] },
-  { slotId: 3, cardId: null, cardData: null, belongCharaIds: [] },
-  { slotId: 4, cardId: null, cardData: null, belongCharaIds: [] },
-  { slotId: 5, cardId: null, cardData: null, belongCharaIds: [] },
+  { slotId: 0, cardId: null, cardData: null, limitBreakStep: 0, belongCharaIds: [] },
+  { slotId: 1, cardId: null, cardData: null, limitBreakStep: 0, belongCharaIds: [] },
+  { slotId: 2, cardId: null, cardData: null, limitBreakStep: 0, belongCharaIds: [] },
+  { slotId: 3, cardId: null, cardData: null, limitBreakStep: 0, belongCharaIds: [] },
+  { slotId: 4, cardId: null, cardData: null, limitBreakStep: 0, belongCharaIds: [] },
+  { slotId: 5, cardId: null, cardData: null, limitBreakStep: 0, belongCharaIds: [] },
 ];
+
 export const cardDeckSlice = createSlice({
   name: 'cardDeck',
   initialState,
   reducers: {
     changeCard: (state, action: PayloadAction<CardSlotType>) => {
       const { slotId, cardId, cardData, belongCharaIds } = action.payload;
-      state[slotId] = { slotId, cardId, cardData, belongCharaIds };
+      state[slotId] = { slotId, cardId, cardData, limitBreakStep: 0, belongCharaIds };
     },
     removeCard: (state, action: PayloadAction<{ slotId: SlotId }>) => {
       const { slotId } = action.payload;
-      state[slotId] = { slotId, cardId: null, cardData: null, belongCharaIds: [] };
+      state[slotId] = {
+        slotId,
+        cardId: null,
+        cardData: null,
+        limitBreakStep: 0,
+        belongCharaIds: [],
+      };
     },
   },
 });
