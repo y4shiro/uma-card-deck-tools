8a28996d95f631a5b8d9fd8c2bc54b421c91aed1
y4shiro <y4shironao@gmail.com>
2022-11-17 17:01:36 +0900

    feat: EffectList にて、サポカ効果ごとに見出しを表示してリスト表示するよう変更

--- src/features/Support/Status/EffectList/index.tsx
+++ src/features/Support/Status/EffectList/index.tsx
@@ -1,4 +1,4 @@
-import { Center, HStack, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Center, HStack, SimpleGrid, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 import React from 'react';
 import { useSelector } from 'react-redux';
@@ -16,40 +16,45 @@ const EffectList: React.FC<Props> = ({ effectList }) => {
   const deck = useSelector((state: RootState) => state.cardDeck);
 
   return (
-    <SimpleGrid w='100%' columns={{ base: 1, sm: 2 }} p='4' gap='2'>
+    <>
       {effectList.map((effect) => {
         return (
           <React.Fragment key={`${effect.name}`}>
-            {[...effect.values].map(([_k, v]) => (
-              <HStack
-                w='100%'
-                p='4'
-                bgColor='white'
-                borderRadius='8'
-                shadow='md'
-                key={`${effect.name},${v.card_id}`}
-              >
-                <Text>{effect.name}</Text>
-                <EffectValue
-                  deck={deck}
-                  card_id={v.card_id}
-                  effect_values={v.effect_values}
-                  unit={effect.unit}
-                />
-                <Center boxSize={{ base: '16px', md: '36px' }}>
-                  <Image
-                    width='64px'
-                    height='64px'
-                    src={`/uma-support-card/card-icons/${v.card_id}.png`}
-                    alt='サポートカードのアイコン'
+            <HStack w='100%' p='4' bgColor='green.300' borderRadius='8'>
+              <Text>{effect.name}</Text>
+            </HStack>
+            <SimpleGrid w='100%' columns={{ base: 1, sm: 2 }} px='4' pt='4' pb='12' gap='2'>
+              {[...effect.values].map(([_k, v]) => (
+                <HStack
+                  w='100%'
+                  p='4'
+                  bgColor='white'
+                  borderRadius='8'
+                  shadow='md'
+                  key={`${effect.name},${v.card_id}`}
+                >
+                  <Text>{effect.name}</Text>
+                  <EffectValue
+                    deck={deck}
+                    card_id={v.card_id}
+                    effect_values={v.effect_values}
+                    unit={effect.unit}
                   />
-                </Center>
-              </HStack>
-            ))}
+                  <Center boxSize={{ base: '16px', md: '36px' }}>
+                    <Image
+                      width='64px'
+                      height='64px'
+                      src={`/uma-support-card/card-icons/${v.card_id}.png`}
+                      alt='サポートカードのアイコン'
+                    />
+                  </Center>
+                </HStack>
+              ))}
+            </SimpleGrid>
           </React.Fragment>
         );
       })}
-    </SimpleGrid>
+    </>
   );
 };
 
