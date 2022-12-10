06dd751e19ea96345bdedaeee05443bab79dc74c
y4shiro <y4shironao@gmail.com>
2022-12-10 17:30:08 +0900

    feat: MultipleObserver コンポーネントのスタイルを調整し、viewport 外で表示するコンポーネントは背景色グレーかつアスペクト比が 3/4 となるよう変更

--- src/features/Support/CardDeck/CardModal/MultipleObserver/index.tsx
+++ src/features/Support/CardDeck/CardModal/MultipleObserver/index.tsx
@@ -1,17 +1,18 @@
-import { Box, Text } from '@chakra-ui/react';
+import { AspectRatio, Box } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 const MultipleObserver: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { ref, inView } = useInView({ triggerOnce: true });
+
   return (
     <div ref={ref}>
       {inView ? (
         children
       ) : (
-        <Box maxW='90px' minH='120px'>
-          <Text>Loading...</Text>
-        </Box>
+        <AspectRatio ratio={3 / 4} bgColor='gray.400' borderRadius='16'>
+          <Box />
+        </AspectRatio>
       )}
     </div>
   );
