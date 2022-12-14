03f081169930c0f5e930609a505326ef4d3b1d0d
y4shiro <y4shironao@gmail.com>
2022-12-14 20:01:24 +0900

    feat: SkillList コンポーネントの見出し (Heading) を EffectLists と共通デザインの物に変更

--- src/features/Support/Skills/SkillList/index.tsx
+++ src/features/Support/Skills/SkillList/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, SimpleGrid, VStack } from '@chakra-ui/react';
+import { Heading, HStack, SimpleGrid, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 import SkillItem from '../SkillItem';
@@ -19,9 +19,17 @@ type Props = {
 const SkillList: React.FC<Props> = ({ heading, skillLists }) => {
   return (
     <VStack w='100%' p='4' gap='2'>
-      <Heading as='h2' fontSize='2xl'>
-        {heading}
-      </Heading>
+      <HStack
+        w='100%'
+        px='4'
+        bgColor='#89c53d'
+        borderRadius='4'
+        clipPath='polygon(0 0, 100% 0%, 98% 100%, 0% 100%)'
+      >
+        <Text fontSize={{ base: 16, sm: 20 }} textColor='white' fontWeight='bold'>
+          {heading}
+        </Text>
+      </HStack>
 
       <SimpleGrid w='100%' columns={{ base: 1, md: 2 }} gap={{ base: '2', md: '4' }}>
         {skillLists?.map((card) =>
