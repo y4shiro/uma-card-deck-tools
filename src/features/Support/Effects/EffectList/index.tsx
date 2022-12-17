9b669ae08725a90576852943502c200ef21b60e8
y4shiro <y4shironao@gmail.com>
2022-12-17 19:02:30 +0900

    refactor: EffectList の余白を調整

--- src/features/Support/Effects/EffectList/index.tsx
+++ src/features/Support/Effects/EffectList/index.tsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { Box, SimpleGrid, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -16,13 +16,13 @@ const EffectList: React.FC<Props> = ({ effectList }) => {
   const deck = useSelector((state: RootState) => state.cardDeck);
 
   return (
-    <Box w='100%' p='4'>
+    <VStack w='100%' p={{ base: 2, md: 4 }} gap='2'>
       {effectList.map((effect) => {
         return (
-          <React.Fragment key={`${effect.name}`}>
+          <Box w='100%' key={`${effect.name}`}>
             <ComponentHeading>{effect.name}</ComponentHeading>
 
-            <SimpleGrid w='100%' columns={{ base: 1, sm: 2 }} px='2' pt='4' pb='12' gap='2'>
+            <SimpleGrid w='100%' columns={{ base: 1, sm: 2 }} p={{ base: 2, md: 4 }} gap='2'>
               {[...effect.values].map(([_k, card]) => (
                 <EffectItem
                   deck={deck}
@@ -32,10 +32,10 @@ const EffectList: React.FC<Props> = ({ effectList }) => {
                 />
               ))}
             </SimpleGrid>
-          </React.Fragment>
+          </Box>
         );
       })}
-    </Box>
+    </VStack>
   );
 };
 
